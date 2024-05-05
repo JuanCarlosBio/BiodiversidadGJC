50825f9d81947bb4fab8af3b0294c3f928dfabb2
JuanCarlos <jcge97@gmail.com>
2024-05-06 01:20:35 +0200

    Nuevas especies + performance gotta go fast

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 La idea es hacer una web con ***GitHub Pages***, para clocar especies en mapas, gráficas con estadísticas etc...
 
-* Para ello usaré ***snakemake*** y ***GitHub Actions***.
-* Las imágenes se descargarán desde mi cuenta de ***roblox***.
+* Para ello usaré ***SANAKEMAKE*** y ***GITHUB ACTIONS***.
+* Las imágenes se descargarán desde mi cuenta de ***DROPBOX***.
 * Los datos pertenecen hasta el momento a invertebrados, flora y vegetación (no descarto otros organismos en un futuro). 
 
 ### **WEB:** [juankkar.github.io/Especies_Encontradas_GC](https://juankkar.github.io/Especies_Encontradas_GC/)
 
-## *Estado del WorkFlow de snakemake:*
+## *Estado del workFlow de snakemake:*
 ![workflow](the_workflow.png)
--- code/04process_exif.R
+++ code/04process_exif.R
@@ -1,9 +1,5 @@
 #!/usr/bin/env Rscript
 
-library(tidyverse)
-library(exifr)
-library(glue)
-
 unzip("images/flora_vascular.zip", exdir = "images/flora_vascular")
 unzip("images/invertebrados.zip", exdir = "images/invertebrados")
 
@@ -12,75 +8,75 @@ ai_files <- list.files("images/invertebrados/",
 fv_files <- list.files("images/flora_vascular/", 
                     full.names = TRUE)
 
-exif_data_ai <- read_exif(ai_files)
-exif_data_fv <- read_exif(fv_files)
+exif_data_ai <- exifr::read_exif(ai_files)
+exif_data_fv <- exifr::read_exif(fv_files)
 
 t_replacement <- c("_ta_" = "á","_te_" = "é","_ti_" = "í", "_to_" = "ó", "_tu_" = "ú", "_enie_" = "ñ")
 
-exif_data_ai %>%
-    rename_all(tolower) %>%
-    select(filename, gpsdatetime, gpsposition,gpsaltitude) %>%
-    mutate(filename = str_replace(filename, pattern = ".jpg", replacement = "")) %>%
-    filter(!(str_detect(filename,  "NO CLASIFICADO")) & str_detect(filename, "^AI")) %>% 
-    separate_wider_delim(gpsposition, delim = " ",
-                         names = c("latitude", "longitude")) %>%
-    mutate(latitude = as.numeric(latitude),
-           longitude = as.numeric(longitude)) %>%
-    separate_wider_delim(filename, delim = "-",
-                         names = c("id", "specie", "author","name", 
-                                   "family", "order", "class", "phylo", "domain", 
-                                   "endemic_genus", "endemic_specie", "endemic_subspecie",
-                                   "origin", "category", "id_biota")) %>% 
-    mutate(endemic_genus = case_when(endemic_genus == "eg_no" ~ "NO",
-                                     endemic_genus == "eg_si" ~ "SI",
-                                     !(endemic_subspecie == "eg_no") | !(endemic_subspecie == "eg_no") ~ "-"),
-           endemic_specie = case_when(endemic_specie == "ee_no" ~ "NO",
-                                      endemic_specie == "ee_si" ~ "SI",
-                                      !(endemic_subspecie == "ee_no") | !(endemic_subspecie == "ee_no") ~ "-"),
-           endemic_subspecie = case_when(endemic_subspecie == "es_no" ~ "NO",
-                                         endemic_subspecie == "es_si" ~ "SI",
-                                         !(endemic_subspecie == "es_no") | !(endemic_subspecie == "es_no") ~ "-"),
-           origin = case_when(origin == "ns" ~ "Nativo seguro", origin == "np" ~ "Nativo probable", 
-                              origin == "isi" ~ "Introducido seguro invasor", origin == "isn" ~ "Introducido seguro no invasor", 
-                              origin == "ip" ~ "Introducido probable"),
-           category = case_when(category == "ep" ~ "Especie protegida",
-                                category == "ei" ~ "Especie introducida",
-                                !(category == "ep") | !(category == "ei") ~ "-"),
-           name = str_replace_all(name, t_replacement),
-           author = str_replace_all(author, t_replacement)) %>% 
-    write_tsv("data/coord_invertebrates.tsv")
+exif_data_ai |>
+    dplyr::rename_all(tolower) |>
+    dplyr::select(filename, gpsdatetime, gpsposition,gpsaltitude) |>
+    dplyr::mutate(filename = stringr::str_replace(filename, pattern = ".jpg", replacement = "")) |>
+    dplyr::filter(!(stringr::str_detect(filename,  "NO CLASIFICADO")) & stringr::str_detect(filename, "^AI")) |>
+    tidyr::separate_wider_delim(gpsposition, delim = " ",
+                                names = c("latitude", "longitude")) |>
+    dplyr::mutate(latitude = as.numeric(latitude),
+                  longitude = as.numeric(longitude)) |>
+    tidyr::separate_wider_delim(filename, delim = "-",
+                                names = c("id", "specie", "author","name", 
+                                          "family", "order", "class", "phylo", "domain", 
+                                          "endemic_genus", "endemic_specie", "endemic_subspecie",
+                                          "origin", "category", "id_biota")) |>
+    dplyr::mutate(endemic_genus = dplyr::case_when(endemic_genus == "eg_no" ~ "NO",
+                                                   endemic_genus == "eg_si" ~ "SI",
+                                                   !(endemic_subspecie == "eg_no") | !(endemic_subspecie == "eg_no") ~ "-"),
+                  endemic_specie = dplyr::case_when(endemic_specie == "ee_no" ~ "NO",
+                                                    endemic_specie == "ee_si" ~ "SI",
+                                                    !(endemic_subspecie == "ee_no") | !(endemic_subspecie == "ee_no") ~ "-"),
+                  endemic_subspecie = dplyr::case_when(endemic_subspecie == "es_no" ~ "NO",
+                                                       endemic_subspecie == "es_si" ~ "SI",
+                                                       !(endemic_subspecie == "es_no") | !(endemic_subspecie == "es_no") ~ "-"),
+                  origin = dplyr::case_when(origin == "ns" ~ "Nativo seguro", origin == "np" ~ "Nativo probable", 
+                                            origin == "isi" ~ "Introducido seguro invasor", origin == "isn" ~ "Introducido seguro no invasor", 
+                                            origin == "ip" ~ "Introducido probable"),
+                  category = dplyr::case_when(category == "ep" ~ "Especie protegida",
+                                              category == "ei" ~ "Especie introducida",
+                                              !(category == "ep") | !(category == "ei") ~ "-"),
+                  name = stringr::str_replace_all(name, t_replacement),
+                  author = stringr::str_replace_all(author, t_replacement)) |>
+    readr::write_tsv("data/coord_invertebrates.tsv")
 
 
-exif_data_fv %>%
-    rename_all(tolower) %>%
-    select(filename, gpsdatetime, gpsposition, gpsaltitude) %>%
-    mutate(filename = str_replace(filename, pattern = ".jpg", replacement = "")) %>%
-    filter(!(str_detect(filename,  "NO CLASIFICADO")) & str_detect(filename, "^FV")) %>%
-    separate_wider_delim(gpsposition, delim = " ",
-                         names = c("latitude", "longitude")) %>%
-    mutate(latitude = as.numeric(latitude),
-           longitude = as.numeric(longitude)) %>%
-    separate_wider_delim(filename, delim = "-",
-                         names = c("id", "specie", "author", "name", "family", 
-                                   "order", "class", "division",
-                                   "endemic_genus", "endemic_specie", "endemic_subspecie",
-                                   "origin", "category", "habitat", "id_biota")) %>%
-    mutate(domain = "plantae",
-           endemic_genus = case_when(endemic_genus == "eg_no" ~ "NO",
-                                     endemic_genus == "eg_si" ~ "SI",
-                                     !(endemic_genus == "eg_no") | !(endemic_genus == "eg_no") ~ "-"),
-           endemic_specie = case_when(endemic_specie == "ee_no" ~ "NO",
-                                      endemic_specie == "ee_si" ~ "SI",
-                                      !(endemic_specie == "ee_no") | !(endemic_specie == "ee_no") ~ "-"),
-           endemic_subspecie = case_when(endemic_subspecie == "es_no" ~ "NO",
-                                         endemic_subspecie == "es_si" ~ "SI",
-                                         !(endemic_subspecie == "es_no") | !(endemic_subspecie == "es_no") ~ "-"),
-           origin = case_when(origin == "ns" ~ "Nativo seguro", origin == "np" ~ "Nativo probable", 
-                              origin == "isi" ~ "Introducido seguro invasor", origin == "isn" ~ "Introducido seguro no invasor", 
-                              origin == "ip" ~ "Introducido probable"),
-           category = case_when(category == "ep" ~ "Especie protegida",
-                                category == "ei" ~ "Especie introducida",
-                                !(endemic_subspecie == "ep") | !(endemic_subspecie == "ei") ~ "-"),    
-           name = str_replace_all(name, t_replacement),
-           author = str_replace_all(author, t_replacement)) %>% 
-    write_tsv("data/coord_plantae.tsv")
+exif_data_fv |>
+    dplyr::rename_all(tolower) |>
+    dplyr::select(filename, gpsdatetime, gpsposition, gpsaltitude) |>
+    dplyr::mutate(filename = stringr::str_replace(filename, pattern = ".jpg", replacement = "")) |>
+    dplyr::filter(!(stringr::str_detect(filename,  "NO CLASIFICADO")) & stringr::str_detect(filename, "^FV")) |>
+    tidyr::separate_wider_delim(gpsposition, delim = " ",
+                                names = c("latitude", "longitude")) |>
+    dplyr::mutate(latitude = as.numeric(latitude),
+                  longitude = as.numeric(longitude)) |>
+    tidyr::separate_wider_delim(filename, delim = "-",
+                                names = c("id", "specie", "author", "name", "family", 
+                                          "order", "class", "division",
+                                          "endemic_genus", "endemic_specie", "endemic_subspecie",
+                                          "origin", "category", "habitat", "id_biota")) |>
+    dplyr::mutate(domain = "plantae",
+                  endemic_genus = dplyr::case_when(endemic_genus == "eg_no" ~ "NO",
+                                                   endemic_genus == "eg_si" ~ "SI",
+                                                   !(endemic_genus == "eg_no") | !(endemic_genus == "eg_no") ~ "-"),
+                  endemic_specie = dplyr::case_when(endemic_specie == "ee_no" ~ "NO",
+                                                    endemic_specie == "ee_si" ~ "SI",
+                                                    !(endemic_specie == "ee_no") | !(endemic_specie == "ee_no") ~ "-"),
+                  endemic_subspecie = dplyr::case_when(endemic_subspecie == "es_no" ~ "NO",
+                                                       endemic_subspecie == "es_si" ~ "SI",
+                                                       !(endemic_subspecie == "es_no") | !(endemic_subspecie == "es_no") ~ "-"),
+                  origin = dplyr::case_when(origin == "ns" ~ "Nativo seguro", origin == "np" ~ "Nativo probable", 
+                                            origin == "isi" ~ "Introducido seguro invasor", origin == "isn" ~ "Introducido seguro no invasor", 
+                                            origin == "ip" ~ "Introducido probable"),
+                  category = dplyr::case_when(category == "ep" ~ "Especie protegida",
+                                              category == "ei" ~ "Especie introducida",
+                                              !(endemic_subspecie == "ep") | !(endemic_subspecie == "ei") ~ "-"),    
+                  name = stringr::str_replace_all(name, t_replacement),
+                  author = stringr::str_replace_all(author, t_replacement)) |> 
+    readr::write_tsv("data/coord_plantae.tsv")
