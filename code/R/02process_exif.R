32e6f8e8d1416999b9c6817319fb64e1ea2a6169
Juan Carlos García Estupiñán <jcge97@gmail.com>
2024-12-10 20:26:37 +0100

    Limpiar el código de 02process_exif.R

--- code/R/02process_exif.R
+++ code/R/02process_exif.R
@@ -8,29 +8,53 @@ suppressMessages(suppressWarnings({
     library(lubridate)
 }))
 
-## -> Ahora que lo vuelvo a visitar a día de 18/11/2024 digo que este código funciona 
-## de maravilla a pesar de ser BASURA xd, tengo pensado como solucionar esta locura 
-## pero necesito tiempo para mentalizarme en cambiar este desastre
+## -> Ahora que lo vuelvo a visitar a día de 18/11/2024 digo que este código 
+## funciona e maravilla a pesar de ser BASURA xd, tengo pensado como solucionar 
+## esta locura pero necesito tiempo para mentalizarme en cambiar este desastre
 
-## -> 24/11/2024, bueno mejorando, pero quedan cosas que limpiar, quiero quitar algún
-## campo más
+## -> 24/11/2024, bueno mejorando, pero quedan cosas que limpiar, quiero quitar 
+## algún campo más
 
-data_biota <- readr::read_tsv("data/biota/processed/biota_data_processed.tsv")
-protected_species_layer <- readr::read_tsv("data/protected_species/coord_plantae_pe.tsv")
+data_biota <- read_tsv("data/biota/processed/biota_data_processed.tsv")
+protected_species_layer <- read_tsv("data/protected_species/coord_plantae_pe.tsv")
 
-rp_species <- read_and_processed_species_spatial <- function(path, id_specie_start){
-
+##==============================================================================
+## INFORMACIÓN:
+## Función para procesar la columna "filename" de los EXIF de las fotogragías 
+## de las especies, ya que son etiquetas con información extra, obtenida a 
+## partir e BIOCAN, Gobierno de Canairas: https://www.biodiversidadcanarias.es/
+## ARGUMENTOS:
+##     path: Ruta donde se encuentran las fotografías
+##     id_specie_start: comienzo del indetificador de las especies (FV=Plantae,
+##                       AI=Metazoa)
+## Resultado:
+##     Data Frame con los datos de los EXIF procesados.
+## EJEMPLO:
+##     rp_species(path = "path/to/speices_plantae.tsv", id_specie_start = "^FV")
+##==============================================================================
+rp_species <- read_and_processed_species_data <- function(path, id_specie_start)
+{
     df_species_processed = read_tsv(path) |>
-        mutate(filename = str_replace(filename, pattern = ".jpg", replacement = "")) |>
+        mutate(
+            filename = str_replace(filename, pattern = ".jpg", replacement = "")
+        ) |>
         filter(
             !(str_detect(filename,  "NO CLASIFICADO")) & 
             str_detect(filename, id_specie_start) & 
             str_count(filename, "-") == 7
-               ) |> 
-        separate_wider_delim(gpsposition, delim = " ", names = c("latitude", "longitude")) |>
-        mutate(latitude = as.numeric(latitude), longitude = as.numeric(longitude)) |>
+        ) |> 
         separate_wider_delim(
-            filename, delim = "-",
+            gpsposition, 
+            delim = " ", 
+            names = c("latitude", "longitude")
+        ) |>
+        mutate(
+            latitude = as.numeric(latitude), 
+            longitude = as.numeric(longitude)
+        ) |>
+        separate_wider_delim(
+            filename, 
+            delim = "-",
             names = c(
                "id", 
                "specie", 
@@ -39,27 +63,35 @@ rp_species <- read_and_processed_species_spatial <- function(path, id_specie_sta
                "endemic_subspecie",
                "origin", 
                "category", 
-               "id_biota")) |>
+               "id_biota"
+            )
+        ) |>
         mutate(
     	endemic_genus = case_when(
                 endemic_genus == "eg_no" ~ "NO",
                 endemic_genus == "eg_si" ~ "SI",
-                !(endemic_subspecie == "eg_no") | !(endemic_subspecie == "eg_no") ~ "-"
+                !(endemic_subspecie == "eg_no") |
+                    !(endemic_subspecie == "eg_no") ~ "-"
     	    ),
             endemic_specie = case_when(
                 endemic_specie == "ee_no" ~ "NO",
                 endemic_specie == "ee_si" ~ "SI",
-                !(endemic_subspecie == "ee_no") | !(endemic_subspecie == "ee_no") ~ "-"
+                !(endemic_subspecie == "ee_no") |
+                    !(endemic_subspecie == "ee_no") ~ "-"
     	    ),
             endemic_subspecie = case_when(
                 endemic_subspecie == "es_no" ~ "NO",
                 endemic_subspecie == "es_si" ~ "SI",
-                !(endemic_subspecie == "es_no") | !(endemic_subspecie == "es_no") ~ "-"
+                !(endemic_subspecie == "es_no") |
+                    !(endemic_subspecie == "es_no") ~ "-"
     	    ),
             origin = case_when(
-                origin == "ns" ~ "Nativo seguro", origin == "np" ~ "Nativo probable", 
-                origin == "isi" ~ "Introducido seguro invasor", origin == "isn" ~ "Introducido seguro no invasor", 
-                origin == "ip" ~ "Introducido probable"
+                origin == "ns" ~ "Nativo seguro", 
+                origin == "np" ~ "Nativo probable", 
+                origin == "isi" ~ "Introducido seguro invasor", 
+                origin == "isn" ~ "Introducido seguro no invasor", 
+                origin == "ip" ~ "Introducido probable",
+                origin == "isp" ~ "Introducido seguro potencialmente invasor"
     	    ),
             category = case_when(
     	        category == "ep" ~ "Especie protegida",
@@ -77,9 +109,14 @@ rp_species <- read_and_processed_species_spatial <- function(path, id_specie_sta
 }
 
 rp_species(
-    path="data/species/raw/raw_dropbox_links_metazoa_content.tsv", id_specie_start="^AI"
-    ) |> readr::write_tsv("data/species/processed/coord_invertebrates.tsv") 
+    path="data/species/raw/raw_dropbox_links_metazoa_content.tsv", 
+    id_specie_start="^AI"
+    ) |> 
+    readr::write_tsv("data/species/processed/coord_invertebrates.tsv") 
 
-rp_species(path="data/species/raw/raw_dropbox_links_plantae_content.tsv", id_specie_start="^FV") |>
+rp_species(
+    path="data/species/raw/raw_dropbox_links_plantae_content.tsv", 
+    id_specie_start="^FV"
+    ) |>
     bind_rows(protected_species_layer) |>
     readr::write_tsv("data/species/processed/coord_plantae.tsv")
